6e37874d9f2255b8607c733ebc76bbc3250090fd
istvandavid97 <79303075+istvandavid97@users.noreply.github.com>
2021-04-23 23:39:32 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Projekt2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projekt2.dir/main.c.obj"
+  "Projekt2.exe"
+  "Projekt2.exe.manifest"
+  "Projekt2.pdb"
+  "libProjekt2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projekt2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
